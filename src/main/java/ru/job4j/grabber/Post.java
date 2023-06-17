3c2c8784ca2d5db9164aca34a1fe083ae98a60e8
KatyaBahai <katyxa-two@yandex.ru>
2023-06-17 19:12:54 +0600

    2.2. Модель данных - Post. [#285211 # [#285211]]

---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,25 @@
+package ru.job4j.grabber;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(title, post.title) && link.equals(post.link) && Objects.equals(description, post.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, link, description);
+    }
+}
